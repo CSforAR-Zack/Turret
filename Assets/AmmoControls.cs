1a61394ab0180bfecf99a8cf3d694f58a626624e
Zackary <zspink@nea.k12.ar.us>
2023-06-30 12:43:06 -0500

    Initial commit

---
+++ Assets/AmmoControls.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoControls : MonoBehaviour
+{
+    public float invTime = 0.1f;
+    public float lifeAfterCollision = 5f;
+    public ParticleSystem explosionPrefab = null;
+    
+    float lifeTime = 0f;
+    bool collided = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        explosionPrefab.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lifeTime += Time.deltaTime;
+        if (collided && lifeTime > lifeAfterCollision)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(lifeTime > invTime)
+        {
+            explosionPrefab.Play();
+            lifeTime = 0f;
+            collided = true;
+            transform.GetComponent<MeshRenderer>().enabled = false;
+            transform.GetComponent<Collider>().enabled = false;
+        }
+    }
+}
