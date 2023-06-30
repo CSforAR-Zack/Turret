fedd1a202a1a68ecdcec3c48623e08ab0f3e0a46
Zackary <zspink@nea.k12.ar.us>
2023-06-30 14:30:04 -0500

    Update

--- Assets/AmmoControls.cs
+++ Assets/AmmoControls.cs
@@ -7,13 +7,16 @@ public class AmmoControls : MonoBehaviour
     public float invTime = 0.1f;
     public float lifeAfterCollision = 5f;
     public ParticleSystem explosionPrefab = null;
+    public AudioClip boom = null;
     
     float lifeTime = 0f;
     bool collided = false;
+    AudioSource audioSource = null;
 
     // Start is called before the first frame update
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
         explosionPrefab.Stop();
     }
 
@@ -32,6 +35,7 @@ public class AmmoControls : MonoBehaviour
         if(lifeTime > invTime)
         {
             explosionPrefab.Play();
+            audioSource.PlayOneShot(boom);
             lifeTime = 0f;
             collided = true;
             transform.GetComponent<MeshRenderer>().enabled = false;
